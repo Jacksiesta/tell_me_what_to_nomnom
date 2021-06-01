ed79a6764115c566f593695bfb9ab3a8d859ef47
Jacksiesta <jacqueline@DESKTOP-5T6VC3M.localdomain>
2021-06-02 00:57:24 +0200

    output looks nicer

--- food.py
+++ food.py
@@ -1,20 +1,19 @@
 import random
 # find way to add/concatenate to my_foods extra meal
 
-# array of easy meals I can prepare
-my_foods = ["spag bol", "pizza", "fried rice"]
-
-# array of ingredients
-ingredients = ['pasta', 'tomato sauce', 'pizza dough', 'white rice', 'courgette', 'carrot']
-
 class meal:
     def __init__(self, name, ingr1, ingr2):
         self.name = name
         self.ingr1 = ingr1
         self.ingr2 = ingr2
+#what if 3rd ingredient?
+
 m = []
 m.append(meal("spag bol", "pasta", "tomato sauce"))
 m.append(meal("pizza", "tomato sauce", "pizza dough"))
 m.append(meal("fried rice", "white rice", "courgette"))
+m.append(meal("Nutella sandwich", "Nutella", "bread"))
 
-print("Hey, you hungy? How does " + m[random.randint(0, 2)].name + " sound?")
+r = random.randint(0, len(m) - 1)
+print("Hey, you hungy? How does " + m[r].name + " sound?\n")
+print("You're gonna need \n - " + m[r].ingr1 + " \n - " + m[r].ingr2)
