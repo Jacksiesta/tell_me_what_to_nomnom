86043b3a58107db2a184d877d77e76a392b1c225
jacksiesta <jacqueline.herrald@gmail.com>
2021-06-02 01:35:41 +0200

    finds food in list and displays possible meal

--- food.py
+++ food.py
@@ -1,6 +1,6 @@
 import random
 # find way to add/concatenate to my_foods extra meal
-
+# create tiers of ingredients - main - secondary - optional
 class meal:
     def __init__(self, name, ingr1, ingr2):
         self.name = name
@@ -14,6 +14,17 @@ m.append(meal("pizza", "tomato sauce", "pizza dough"))
 m.append(meal("fried rice", "white rice", "courgette"))
 m.append(meal("Nutella sandwich", "Nutella", "bread"))
 
-r = random.randint(0, len(m) - 1)
-print("Hey, you hungy? How does " + m[r].name + " sound?\n")
-print("You're gonna need \n - " + m[r].ingr1 + " \n - " + m[r].ingr2)
+### WORKS
+#r = random.randint(0, len(m) - 1)
+#print("Hey, you hungy? How does " + m[r].name + " sound?\n")
+#print("You're gonna need \n - " + m[r].ingr1 + " \n - " + m[r].ingr2)
+
+# it'd be cool to input an ingredient, and output suggestion meal
+food = input("What ingredient do you have?\n -> ").lower()
+for i in range(0, len(m)):
+    if m[i].ingr1 == food:
+        print("You can make {}".format(m[i].name))
+        break
+    elif m[i].ingr2 == food:
+        print("You can make {}".format(m[i].name))
+        break
